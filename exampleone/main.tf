d178b72d09b9bf750a3b78bda1a1667381bb90f2
suneelbabuv <107174156+suneelbabuv@users.noreply.github.com>
2022-07-04 19:13:15 +0530

    Create main.tf

---
+++ exampleone/main.tf
@@ -0,0 +1,16 @@
+terraform { 
+  backend "azure" {} 
+  }
+
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=3.0.0"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "RGfromCICD"
+  location = "East US"
+}
